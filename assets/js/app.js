54dce348de525555bf201afaee5c6457f1b38971
Paulina Murzanska <p.murzanska@gmail.com>
2021-04-21 20:50:29 +0200

    some  code clear

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,32 +16,6 @@ $('.slider .owl-carousel').owlCarousel({
 
 })
 
-// $('.testimonials .owl-carousel').owlCarousel({
-//     loop: true,
-//     margin: 20,
-//     nav: true,
-//     responsive: {
-//         0: {
-//             items: 1
-//         },
-//         600: {
-//             items: 2
-//         },
-//         1000: {
-//             items: 4
-//         }
-//     }
-
-// })
-// AOS.init();
-
-// $(window).on('scroll', function () {
-//     if ($(document).scrollTop() > 100) {
-//         $('.navigation').addClass('move');
-//     } else {
-//         $('.navigation').removeClass('move');
-//     }
-// })
 
 $('.hamburger').on('click', (event) => {
     $('.hamburger').toggleClass('is-active');
